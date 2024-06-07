cfd520b14a2c19475a3ae2e55c76ae74eb4cb289
Varun Kanna <73306137+varun-kanna@users.noreply.github.com>
2024-06-07 15:44:22 -0700

    utilized more material ui for todos.js

--- src/component/todos.js
+++ src/component/todos.js
@@ -25,7 +25,10 @@ const Todos = ({ todos }) => {
 						{/* Remember, we set the local state of this todo item when the user submits the form in 
               AddTodo.js. All we need to do is return the todo list item */}
 						<CardContent>
-							<span style={{ padding: '50px' }}>{todo}</span>
+							<ListItemButton>
+								<Checkbox checked={todo.completed} />
+								<ListItemText primary={todo.content} />
+							</ListItemButton>
 						</CardContent>
 					</Card>
 				</Grid>
