8354f3250b7df415df87e2f33eecfe79392b3786
Varun Kanna <73306137+varun-kanna@users.noreply.github.com>
2024-06-07 15:34:35 -0700

    passing on IDE, but still some bugs to fix

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
-  
+import './About.css';
+import profile from '../assets/profile.jpg';
+
 export default class About extends Component {
-  render() {
-    return (
-      <div>
-        <p>Design your About me page </p> 
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<div>
+					<div className='split left'>
+						<div className='centered'>
+							<img
+								className='profile_image'
+								src={profile}
+								alt='Profile Pic'></img>
+						</div>
+					</div>
+					<div className='split right'>
+						<div className='centered'>
+							<div className='name_title'>Varun Kanna</div>
+							<div className='brief_description'>
+								<ul>
+									<li>I like sports</li>
+									<li>I like Fullstack</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
